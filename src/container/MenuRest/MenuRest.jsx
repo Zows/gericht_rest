4f1cb9dba8c01a6c91e747cd1d02266d390a93e8
Zows <vyudchenko@gmail.com>
2022-07-11 09:52:35 +0300

    Added MenuRest

--- src/container/MenuRest/MenuRest.jsx
+++ src/container/MenuRest/MenuRest.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './MenuRest.css';
 
 const MenuRest = () => (
-	<div>
-		MenuRest
+	<div className="app__bg-menu">
+		<div className="app__logo-left">
+			<img src={images.logo} alt="app_logo"/>
+		</div>
+		<div className="app__menu-bar">
+			<h1 className="headtext__cormorant-menu"><a href="#" className="underline">Bar Menu</a></h1>
+			<h1 className="headtext__cormorant-menu"><a href="#" className="underline">Food Menu</a></h1>
+			<h1 className="headtext__cormorant-menu"><a href="#" className="underline">Desserts Menu</a></h1>
+		</div>
 	</div>
 );
 
